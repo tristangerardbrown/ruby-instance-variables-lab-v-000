3611806c88e27b407e5281b19419d8f8c0910ac8
Tristan Brown <tgbrown@stanford.edu>
2018-08-31 07:47:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def name = (dog_name)
+  def name=(dog_name)
     this_dogs_name = dog_name
   end
 
@@ -11,4 +11,4 @@ end
 lassie = Dog.new
 lassie.name = "Lassie"
 
-lassie.name 
+lassie.name
